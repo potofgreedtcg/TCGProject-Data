83820bdd83e8120fd04c896242d9ae0b95c7b24c
asfandyar khan <mrasfankhan@gmail.com>
2025-05-22 22:46:48 -0400

    Update

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"time"
-	// os "os"
-	// "strconv"
+	os "os"
+	"strconv"
 
-	// "github.com/potofgreedtcg/TCGProject-Data/dataParser"
-	"github.com/potofgreedtcg/TCGProject-Data/git"
+	"github.com/potofgreedtcg/TCGProject-Data/dataParser"
 )
 
 func main() {
@@ -15,30 +14,30 @@ func main() {
 	start := time.Now()
     fmt.Println("========= Starting Data Updating =========")
 
-	// gameCsvData, err := dataParser.GetGameData()
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	os.Exit(1)
-	// }
-	// setCsvData, err := dataParser.GetSetData(strconv.Itoa(3))
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	os.Exit(1)
-	// }
+	gameCsvData, err := dataParser.GetGameData()
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	setCsvData, err := dataParser.GetSetData(strconv.Itoa(3))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-	// fmt.Printf("Game Data: %d\nSet Data: %d\n", len(gameCsvData), len(setCsvData))
+	fmt.Printf("Game Data: %d\n Set Data: %d\n", len(gameCsvData), len(setCsvData))
 
-	// for _, set := range setCsvData {
-	// 	fmt.Printf("Product Data: %d - %d \n", set.CategoryId, set.GroupId)
-	// 	productCsvData, err := dataParser.GetProductData(strconv.Itoa(set.CategoryId), strconv.Itoa(set.GroupId))
-	// 	if err != nil {
-	// 		fmt.Println("Error:", err)
-	// 		os.Exit(1)
-	// 	}
-	// 	fmt.Printf("Product Items: %d \n", len(productCsvData))
-	// }
+	for _, set := range setCsvData {
+		fmt.Printf("Product Data: %d - %d \n", set.CategoryId, set.GroupId)
+		productCsvData, err := dataParser.GetProductData(strconv.Itoa(set.CategoryId), strconv.Itoa(set.GroupId))
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Product Items: %d \n", len(productCsvData))
+	}
 	
-	git.GitCommit()
+	
 	fmt.Println("========= Finished Data Updating =========")
 	duration := time.Since(start)
 	hours := int(duration.Hours())
